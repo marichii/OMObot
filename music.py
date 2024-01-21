5afdbfed4d0a442afdfa8930c95c2a50f8727f43
marichii <92391405+marichii@users.noreply.github.com>
2024-01-20 20:30:23 -0500

    fixed comment outs
    oopsies

--- music.py
+++ music.py
@@ -4,49 +4,49 @@ import youtube_dl
 import asyncio
 import os
 
-# class music(commands.Cog):
-#     def __init__(self, client):
-#         self.client = client
+class music(commands.Cog):
+    def __init__(self, client):
+        self.client = client
 
-#     @commands.command()
-#     async def join(self,ctx):
-#         if ctx.author.voice is None:
-#             await ctx.send("Get in a voice channel, loser.")
-#         voice_channel = ctx.author.voice.channel
-#         if ctx.voice_client is None:
-#             await voice_channel.connect()
-#         else:
-#             await ctx.voice_client.move_to(voice_channel)
-#     @commands.command()
-#     async def disconnect(self,ctx):
-#         await ctx.voice_client.disconnect()
+    @commands.command()
+    async def join(self,ctx):
+        if ctx.author.voice is None:
+            await ctx.send("Get in a voice channel, loser.")
+        voice_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
+    @commands.command()
+    async def disconnect(self,ctx):
+        await ctx.voice_client.disconnect()
 
-#     #here is where the music plays
-#     @commands.command()
-#     async def play(self,ctx,url):
-#         ctx.voice_client.stop()
-#         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-#         YDL_OPTIONS = {'format':"bestaudio"}
-#         vc = ctx.voice_client
+    #here is where the music plays
+    @commands.command()
+    async def play(self,ctx,url):
+        ctx.voice_client.stop()
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        YDL_OPTIONS = {'format':"bestaudio"}
+        vc = ctx.voice_client
 
-#         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-#             info = ydl.extract_info(url, download=False)
-#             url2 = info['formats'][0]['url']
-#             source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-#             vc.play(source)
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            vc.play(source)
 
-#     @commands.command()
-#     async def pause(self,ctx):
-#         await ctx.voice_client.pause()
-#         await ctx.send("Paused.")
+    @commands.command()
+    async def pause(self,ctx):
+        await ctx.voice_client.pause()
+        await ctx.send("Paused.")
 
-#     @commands.command()
-#     async def resume(self,ctx):
-#         await ctx.voice_client.resume()
-#         await ctx.send("Resumed")
+    @commands.command()
+    async def resume(self,ctx):
+        await ctx.voice_client.resume()
+        await ctx.send("Resumed")
 
-# def setup(client):
-#     client.add_cog(music(client))
+def setup(client):
+    client.add_cog(music(client))
 
 
 
